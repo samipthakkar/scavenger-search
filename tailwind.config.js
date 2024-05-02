347162c5721cc27ea984e8ab7ea9c1f1ad77d308
samipthakkar <samipthakkarr44@gmail.com>
2024-05-02 00:11:22 -0700

    Directory setup, installed and configured tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['./public/index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  //purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  // variants: {
+  //   extend: {},
+  // },
+  plugins: [],
+}
