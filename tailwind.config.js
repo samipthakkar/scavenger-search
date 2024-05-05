78bff5de9715ea1a7bd05aa80a505450603db58e
samipthakkar <samipthakkarr44@gmail.com>
2024-05-05 16:23:58 -0700

    Added toggle for Light/Dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
 module.exports = {
   content: ['./public/index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  //purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: 'media', // or 'media' or 'false'
   theme: {
     extend: {},
   },
-  // variants: {
-  //   extend: {},
-  // },
-  plugins: [],
+  plugins: [
+    require('tailwindcss-dark-mode')()
+  ],
+  // To enable dark mode for all classes:
+  variants: ['dark', 'dark-hover', 'dark-group-hover', 'dark-even', 'dark-odd']
 }
